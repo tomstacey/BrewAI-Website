045fb9ff637b875b929fcb208d996e4592b3847c
tomstacey <tomstacey@gmail.com>
2026-04-15 15:51:33 +0100

    change staff order again in app

--- js/app.js
+++ js/app.js
@@ -29,16 +29,16 @@ createApp({
 ],
            team: [
     {
-        name: 'Dr Lewis Walsh',
-        title: '',
-        photo: 'lewis.jpg',
-        linkedin: 'https://www.linkedin.com/in/lewis-walsh-0471a14a/'
-    },
-    {
         name: 'Dr Franziska Sohns',
         title: '',
         photo: 'franzi.jpg',
         linkedin: 'https://www.linkedin.com/in/dr-franziska-sohns-9564509a/'
+    },
+    {
+        name: 'Dr Lewis Walsh',
+        title: '',
+        photo: 'lewis.jpg',
+        linkedin: 'https://www.linkedin.com/in/lewis-walsh-0471a14a/'
     },
     {
         name: 'Tom Stacey',
